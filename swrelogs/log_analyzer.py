98481aadbd6cc27d76aa8c86a71204350d459eb2
Sergio Botero <sergiobuj@users.noreply.github.com>
2022-12-01 16:39:00 -0500

    Extract metric collectors to their own classes
    I need a way to add metric collectors and standardize the collection
    method.

--- swrelogs/log_analyzer.py
+++ swrelogs/log_analyzer.py
@@ -1,9 +1,8 @@
-from collections import Counter
-from datetime import datetime
 from io import BufferedReader
 
 from dateutil import tz
 
+from .metrics import BytesCounter, EventRate, IPCounter
 from .readers.squid import SquidLogReader
 
 
@@ -12,28 +11,24 @@ def _get_log_reader(file: BufferedReader):
     return SquidLogReader
 
 
-def log_analyzer(source_filepath: str) -> dict[str, float | int | str]:
-    bytes_total = 0
-    ip_counter = Counter()
-    log_entries_count = 0
-    min_timestamp, max_timestamp = float("Inf"), float("-Inf")
+def log_analyzer(
+    source_filepath: str, *args, **kwargs
+) -> dict[str, dict | float | int | str]:
+    event_rate_metric = EventRate()
+    ip_count_metric = IPCounter()
+    total_bytes_metric = BytesCounter()
+
+    metrics = [event_rate_metric, ip_count_metric, total_bytes_metric]
 
     with open(source_filepath, "rb") as file:
         reader = _get_log_reader(file)
         for entry in reader(file).logs():
-            log_entries_count += 1
-            ip_counter.update([entry.remotehost])
-            bytes_total += int(entry.bytes)
-            min_timestamp = min(min_timestamp, float(entry.timestamp))
-            max_timestamp = max(max_timestamp, float(entry.timestamp))
+            for metric in metrics:
+                metric.update(entry)
 
-
-    min_datetime = datetime.fromtimestamp(min_timestamp, tz=tz.UTC)
-    max_datetime = datetime.fromtimestamp(max_timestamp, tz=tz.UTC)
-    diff_datetime = max_datetime - min_datetime
     return {
-        "mfip": ip_counter.most_common(1)[0][0],
-        "lfip": ip_counter.most_common()[-1][0],
-        "eps": log_entries_count / diff_datetime.total_seconds(),
-        "bytes": bytes_total,
+        "mfip": ip_count_metric.report(),
+        "lfip": ip_count_metric.report(lfip=True),
+        "eps": event_rate_metric.report(),
+        "bytes": total_bytes_metric.report(),
     }
