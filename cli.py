7b06374c2939af4f68d64abaec56c4b6848185aa
Sergio Botero <sergiobuj@users.noreply.github.com>
2022-12-02 23:52:38 -0500

    Move out metric instantiation
    In order to support multifile processing we need to extract the process
    that extracts the metrics from the output preparation.
    
    The log_analyzer function is becoming less cluttered and is slowly
    taking less responsibilities.

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 import argparse
 
 from swrelogs.log_analyzer import log_analyzer
+from swrelogs.metrics import BytesCounter, EventRate, IPCounter
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -13,7 +14,58 @@ if __name__ == "__main__":
         help="Path to one plain-text file, or a directory. If directory, take all files as input.",
         type=str,
     )
+
+    parser.add_argument(
+        "--mfip",
+        action=argparse.BooleanOptionalAction,
+        default=False,
+        help="Most frequent IP",
+        type=bool,
+    )
+
+    parser.add_argument(
+        "--lfip",
+        action=argparse.BooleanOptionalAction,
+        type=bool,
+        help="Least frequent IP",
+        default=False,
+    )
+
+    parser.add_argument(
+        "--eps",
+        action=argparse.BooleanOptionalAction,
+        type=bool,
+        help="Events per second",
+        default=False,
+    )
+
+    parser.add_argument(
+        "--bytes",
+        action=argparse.BooleanOptionalAction,
+        type=bool,
+        help="Total amount of bytes exchanged",
+        default=False,
+    )
     arguments = parser.parse_args()
 
-    report = log_analyzer(arguments.input)
-    print(report)
+    metrics = []
+    if arguments.mfip:
+        metrics.append(IPCounter())
+
+    if arguments.lfip:
+        metrics.append(IPCounter(least_frequent=True))
+
+    if arguments.eps:
+        metrics.append(EventRate())
+
+    if arguments.bytes:
+        metrics.append(BytesCounter())
+
+    if metrics:
+        log_analyzer(arguments.input, metrics)
+
+        report = {}
+        for metric in metrics:
+            label = metric.label()
+            report[label] = metric.report()
+        print(report)
--- swrelogs/log_analyzer.py
+++ swrelogs/log_analyzer.py
@@ -1,6 +1,7 @@
 from io import BufferedReader
+from typing import List
 
-from .metrics import BytesCounter, EventRate, IPCounter
+from .metrics import MetricBase
 from .readers.squid import SquidLogReader
 
 
@@ -9,24 +10,9 @@ def _get_log_reader(file: BufferedReader):
     return SquidLogReader
 
 
-def log_analyzer(
-    source_filepath: str, *args, **kwargs
-) -> dict[str, dict | float | int | str]:
-    event_rate_metric = EventRate()
-    ip_count_metric = IPCounter()
-    total_bytes_metric = BytesCounter()
-
-    metrics = [event_rate_metric, ip_count_metric, total_bytes_metric]
-
+def log_analyzer(source_filepath: str, metrics: List[MetricBase]) -> None:
     with open(source_filepath, "rb") as file:
         reader = _get_log_reader(file)
         for entry in reader(file).logs():
             for metric in metrics:
                 metric.update(entry)
-
-    return {
-        "mfip": ip_count_metric.report(),
-        "lfip": ip_count_metric.report(lfip=True),
-        "eps": event_rate_metric.report(),
-        "bytes": total_bytes_metric.report(),
-    }
